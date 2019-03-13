e44acd23bd3d1fcd844823d1a4d514b5d8bcd7e9
jfcameron <jf.cameron@gmail.com>
2019-03-12 22:57:09 -0400

    wip

--- workspace/build.sh
+++ workspace/build.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 
+# Demo
+DEMO_SOURCE_ROOT_DIR="../demo"
+
+# Lib
 SOURCE_ROOT_DIR="../src"
 PUBLIC_HEADER_DIR="../include"
 PRIVATE_HEADER_DIR="${SOURCE_ROOT_DIR}/include"
 
 g++ \
+    "${DEMO_SOURCE_ROOT_DIR}"/*.cpp \
     "${SOURCE_ROOT_DIR}"/*.cpp \
     -I"${PUBLIC_HEADER_DIR}" \
     -I"${PRIVATE_HEADER_DIR}" \
